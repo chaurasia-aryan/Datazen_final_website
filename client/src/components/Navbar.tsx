5264bf41ba61ae20eb1d6dd7fbfd400a956c7306
chaurasia-aryan <chaurasiaaryan1980@gmail.com>
2025-05-22 20:30:36 +0530

    added href

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link, useLocation } from "wouter";
